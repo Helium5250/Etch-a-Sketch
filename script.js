4c42d3ede0cb85d695074d3fc8286b55c8e7dd84
codicate <codicate@gmail.com>
2021-02-20 22:13:03 -0500

    Added change size button

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 let SizeBtn = document.querySelector('#size');
 let canvas = document.querySelector('#canvas');
 
-let size = 16;
-for (let i = 0; i < Math.pow(size, 2); i++) {
-    let square = document.createElement('div');
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "darkblue";
-    })
-    canvas.append(square);
+SizeBtn.onclick = () => {
+    let size = prompt("Enter size:");
+    if (!isNaN(size)) {
+        createGrid(size);
+        console.log(typeof parseInt(size));
+        console.log(parseInt(size));
+    }
+    else {
+        console.log("f");
+        alert("Not a number");
+    }
+};
+
+function createGrid(size) {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
+
+    for (let i = 0; i < Math.pow(size, 2); i++) {
+        let square = document.createElement('div');
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "darkblue";
+        })
+        canvas.append(square);
+    }
+
+    canvas.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, auto)`;
 }
 
-canvas.style.gridTemplateColumns = `repeat(${size}, auto)`;
-canvas.style.gridTemplateRows = `repeat(${size}, auto)`;
+createGrid(16);
+
