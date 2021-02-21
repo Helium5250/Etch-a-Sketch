7323cbb59959c96f5c05d9a52872233f1616dde5
codicate <codicate@gmail.com>
2021-02-20 23:01:52 -0500

    erase, clear, color picker btns

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let SizeBtn = document.querySelector('#size');
 let canvas = document.querySelector('#canvas');
+let clearBtn = document.querySelector('#clear');
+let sizeBtn = document.querySelector('#size');
+let eraserBtn = document.querySelector('#eraser');
+let colorBtn = document.querySelector('#color');
 
-SizeBtn.onclick = () => {
+const clear = toHSLA(0, 100, 100);
+let color = toHSLA(0, 100, 0);
+
+function toHSLA(hue, saturation, lightness = 100, alpha = 1.0) {
+    return "hsla(" + hue + "," + saturation + "%," + lightness + "%," + alpha + ")"
+}
+
+clearBtn.onclick = () => {
+    for (square of canvas.childNodes) {
+        square.style.backgroundColor = clear;
+    }
+};
+
+eraserBtn.onclick = () => color = clear;
+
+colorBtn.addEventListener("input", () => {
+    color = colorBtn.value;
+});
+
+sizeBtn.onclick = () => {
     let size = prompt("Enter size:");
     if (!isNaN(size)) {
         createGrid(size);
@@ -22,8 +44,8 @@ function createGrid(size) {
     for (let i = 0; i < Math.pow(size, 2); i++) {
         let square = document.createElement('div');
         square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = "darkblue";
-        })
+            square.style.backgroundColor = color;
+        });
         canvas.append(square);
     }
 
