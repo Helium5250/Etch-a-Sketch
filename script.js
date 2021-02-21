c38ab0faa3f3d6aa7cedf3dc87a0ae73d83e5d4f
codicate <codicate@gmail.com>
2021-02-20 21:45:15 -0500

    Fixed grid,  box color change on hover

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ let canvas = document.querySelector('#canvas');
 let size = 16;
 for (let i = 0; i < Math.pow(size, 2); i++) {
     let square = document.createElement('div');
+    square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "darkblue";
+    })
     canvas.append(square);
 }
+
+canvas.style.gridTemplateColumns = `repeat(${size}, auto)`;
+canvas.style.gridTemplateRows = `repeat(${size}, auto)`;
